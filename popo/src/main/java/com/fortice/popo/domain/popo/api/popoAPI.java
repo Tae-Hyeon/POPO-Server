02edaf32647778a8a8bf5cdcab8f52b179bf3a20
한태현 <hanth7028@gmail.com>
2021-08-16 15:54:30 +0900

    feat: Add popo API, popo JPA test
    DAO에 작성한 모델을 /domain/model로 이동
    JPA SELECT 기능 테스트 진행
    /popo GET 요청 테스트 진행

--- popo/src/main/java/com/fortice/popo/domain/popo/api/popoAPI.java
+++ popo/src/main/java/com/fortice/popo/domain/popo/api/popoAPI.java
@@ -1,4 +1,44 @@
 package com.fortice.popo.domain.popo.api;
 
+import com.fortice.popo.domain.model.Popo;
+import com.fortice.popo.domain.popo.dao.PopoDAO;
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+@RestController
+@RequestMapping(value = "/popo", produces = {MediaType.APPLICATION_JSON_VALUE})
 public class popoAPI {
+    @Autowired
+    PopoDAO popoDAO;
+
+    @RequestMapping(method= RequestMethod.GET)
+    public @ResponseBody Object getPopoList() {
+        List<Popo> popoList = popoDAO.findAll();
+
+        return popoList;
+    }
+
+    @RequestMapping(value = "/{popoId}", method= RequestMethod.GET)
+    public @ResponseBody Object getPopo(@PathVariable("popoId") Long popoId) {
+        Optional<Popo> popo = popoDAO.findById(popoId);
+
+        return popo;
+    }
+
+    @RequestMapping(value = "/popo", method = RequestMethod.POST)
+    public @ResponseBody Object insertPopo(@RequestBody Popo newPopo) {
+        popoDAO.save(newPopo);
+        return newPopo;
+    }
+
+    @RequestMapping(value = "/popo/{popoId}", method = RequestMethod.PATCH)
+    public @ResponseBody Object changeBackground(@ResponseBody)
 }
--- popo/src/test/java/com/fortice/popo/PopoApplicationTests.java
+++ popo/src/test/java/com/fortice/popo/PopoApplicationTests.java
@@ -1,13 +1,19 @@
 package com.fortice.popo;
 
+import com.fortice.popo.jpa.PopoJPATest;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.web.WebAppConfiguration;
 
 @SpringBootTest
+@WebAppConfiguration
 class PopoApplicationTests {
 
 	@Test
 	void contextLoads() {
-	}
+		PopoJPATest jpaTest = new PopoJPATest();
+		jpaTest.getListTest();
+		jpaTest.getPopoByIdTest();
 
+	}
 }
